a04b3ca8e4d6fbac28abb0c609bb0be194c98d0d
Saksham jamwal <sakshamjamwal723@gmail.com>
2024-06-12 20:44:14 +0530

    added

---
+++ app.js
@@ -0,0 +1,17 @@
+const parent = React.createElement("div", { id: 'parent' },
+    [
+        React.createElement('div', { id: 'child1' },
+            [
+                React.createElement('h1', {}, 'this is a h1 tag'),
+                React.createElement('h2', {}, 'This is a h2 tag')
+            ]),
+        React.createElement('div', { id: 'child2' },
+            [
+                React.createElement('h1', {}, 'this is a h1 tag'),
+                React.createElement('h2', {}, 'This is a h2 tag')
+            ])
+    ]
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent)
